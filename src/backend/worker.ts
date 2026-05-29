9098bba9ed0a675f31fd84c073266b31232f1c07
jainashish0712 <jainashish0712@gmail.com>
2026-05-29 19:46:37 +0530

    added local host address to access from phone

--- src/backend/worker.ts
+++ src/backend/worker.ts
@@ -19,6 +19,7 @@ const ALLOWED_ORIGINS = [
   'https://try-this.vercel.app',
   'http://localhost:3000',
   'http://localhost:5173',
+  'http://192.168.88.13:5173',
   'https://meq3d.netlify.app'
 ];
 
